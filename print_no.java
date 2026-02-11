f1503a1dbfbef4d0e0da69364196d210f7cd68e0
rahul-pandey-20330 <rahulpandeyr184@gmail.com>
2026-02-11 22:48:53 +0530

    Ap Question Solved

---
+++ print_no.java
@@ -0,0 +1,13 @@
+package loops;
+
+// Ques: Print numbers from 1 to 100
+import java.util.Scanner;
+
+public class print_no {
+
+    public static void main(String[] args) {
+        for (int i = 1 ;i<=100 ; i++){
+            System.out.println(i);
+        }
+    }
+}
